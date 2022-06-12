ed1bb13e3d09060afbfe454c141bc0bc1aa4b8bc
Wilson Oh <e0773510@u.nus.edu>
2022-06-13 01:01:33 +0800

    use vim-bbye's Bdelete instead

--- lua/bufferline-config/init.lua
+++ lua/bufferline-config/init.lua
@@ -1,7 +1,20 @@
 local bufferline = Vapour.utils.plugins.require('bufferline')
 bufferline.setup {
   options = {
-    separator_style = "slant",
+    close_command = "Bdelete! %d",
+    right_mouse_command = "Bdelete! %d",
+    separator_style = "thick",
+    --[[ diagnostics = "nvim_lsp",
+
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local s = " "
+      for e, n in pairs(diagnostics_dict) do
+        local sym = e == "error" and " "
+            or (e == "warning" and " " or "")
+        s = s .. n .. sym
+      end
+      return s
+    end, ]]
     offsets = {
       {
         filetype = "NvimTree",
