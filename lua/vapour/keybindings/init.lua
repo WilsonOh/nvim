66a7b7b4d935b642d4173ee584bb19c64cd23abd
Wilson Oh <e0773510@u.nus.edu>
2022-06-13 01:09:19 +0800

    added keybinding for sourcing the current buffer

--- lua/vapour/keybindings/init.lua
+++ lua/vapour/keybindings/init.lua
@@ -70,6 +70,9 @@ set('i', '<C-a>', '<esc>I', opts)
 set('n', 'gd', '0D', opts)
 set('n', 'gC', '0C', opts)
 
+--Source Current Buffer
+set('n', '<leader><leader>s', '<cmd>%so<CR>', opts)
+
 if Vapour.plugins.nvim_comment.enabled then set('v', '<leader>/', ':CommentToggle<cr>', opts) end
 
 vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
