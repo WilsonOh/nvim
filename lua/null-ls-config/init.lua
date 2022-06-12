ce05aac0aa3213f38f5af89b888905f634effa1a
Wilson Oh <e0773510@u.nus.edu>
2022-06-13 01:07:07 +0800

    organize the different source methods

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -1,20 +1,32 @@
 local null_ls = Vapour.utils.plugins.require('null-ls')
 
+if not null_ls then return end
+
 local formatting = null_ls.builtins.formatting
+
+local diag = null_ls.builtins.diagnostics
+
+local code_action = null_ls.builtins.code_actions
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local formatting_sources = {
+  formatting.prettier, formatting.gofmt, formatting.lua_format.with({
+    extra_args = {
+      '--no-keep-simple-function-one-line', '--no-break-after-operator', '--column-limit=100',
+      '--break-after-table-lb', '--indent-width=2'
+    }
+  }), formatting.black, formatting.eslint_d
+}
+
+local diagnostic_sources = { diag.codespell, diag.flake8, diag.eslint_d }
+
+local code_action_sources = { code_action.eslint_d }
+
+local sources = vim.tbl_extend("force", formatting_sources, diagnostic_sources, code_action_sources)
+
 null_ls.setup({
-  sources = {
-    formatting.prettier, formatting.black, formatting.gofmt, formatting.shfmt,
-    formatting.cmake_format, formatting.dart_format,
-    formatting.lua_format.with({
-      extra_args = {
-        '--no-keep-simple-function-one-line', '--no-break-after-operator', '--column-limit=100',
-        '--break-after-table-lb', '--indent-width=2'
-      }
-    }), formatting.isort, formatting.codespell.with({ filetypes = { 'markdown' } })
-  },
+  sources = sources,
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
@@ -22,8 +34,10 @@ null_ls.setup({
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
+          -- For neovim 0.8.0
+          -- vim.lsp.buf.format({ bufnr = bufnr })
+          vim.lsp.buf.formatting_seq_sync()
+        end
       })
     end
   end
