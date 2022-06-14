0f40316d5d4ce6ce9bb35251a2cb4f8ff2fa7bbd
WilsonOh <oh.wilson123@gmail.com>
2022-06-14 12:38:48 +0800

    removed BufRead autocommand from plugin loading, added custom lsp_utils file which uses a custom filter and prints out a formatting message

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -11,17 +11,17 @@ local code_action = null_ls.builtins.code_actions
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local formatting_sources = {
-  --[[ formatting.prettier, formatting.gofmt, formatting.lua_format.with({
+  formatting.prettier, formatting.gofmt, formatting.lua_format.with({
     extra_args = {
       '--no-keep-simple-function-one-line', '--no-break-after-operator', '--column-limit=100',
       '--break-after-table-lb', '--indent-width=2'
     }
-  }), formatting.black, formatting.eslint_d ]]
+  }), formatting.black, formatting.eslint_d
 }
 
 local diagnostic_sources = { --[[  diag.codespell, diag.flake8, diag.eslint_d  ]] }
 
-local code_action_sources = { code_action.eslint_d }
+local code_action_sources = {code_action.eslint_d}
 
 local sources = vim.tbl_extend("force", formatting_sources, diagnostic_sources, code_action_sources)
 
@@ -29,7 +29,7 @@ null_ls.setup({
   sources = sources,
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_clear_autocmds({group = augroup, buffer = bufnr})
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
