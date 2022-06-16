ecf4cabb34f91487cbfdc0ae97d461ff52b31842
Wilson Oh <e0773510@u.nus.edu>
2022-06-17 00:11:59 +0800

    don't commit ftplugin to repo

--- ftplugin/qf.lua
+++
@@ -1,8 +0,0 @@
-local set = vim.keymap.set
-local opts = { silent = true }
-
-set('n', '<leader>c', '<cmd>cclose<CR>', opts)
-set('n', '<leader>n', '<cmd>cnext<CR><cmd>copen<CR>', opts)
-set('n', '<leader>N', '<cmd>cprev<CR><cmd>copen<CR>', opts)
-set('n', '<leader>F', '<cmd>cfirst<CR><cmd>copen<CR>', opts)
-set('n', '<leader>L', '<cmd>clast<CR><cmd>copen<CR>', opts)
