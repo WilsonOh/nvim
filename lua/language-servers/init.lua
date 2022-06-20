8f10bd42ded73998066f0535e96cb8abdd54a1cb
WilsonOh <oh.wilson123@gmail.com>
2022-06-20 11:15:20 +0800

    always show source from virtual text diagnostics

--- lua/language-servers/init.lua
+++ lua/language-servers/init.lua
@@ -69,6 +69,15 @@ end
 
 local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
 
+vim.diagnostic.config({
+  virtual_text = {
+    source = 'always', -- Or "if_many"
+  },
+  float = {
+    source = 'always', -- Or "if_many"
+  },
+})
+
 for type, icon in pairs(signs) do
   local hl = 'DiagnosticSign' .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
