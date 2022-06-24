bb6ff0eeea6e4ed1bb680cebd94a33c823ba9e61
WilsonOh <oh.wilson123@gmail.com>
2022-06-24 16:37:15 +0800

    added vim-markdown; made nvim-jdtls config more dynamic

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,6 +3,10 @@ local workspace_dir = '/Users/wilsonoh/coding_stuff/java_projects/workspace/' ..
 
 local home = os.getenv('HOME')
 
+local current_os = io.popen('uname'):read('*a')
+
+local config_dir = current_os == 'Darwin' and 'config_mac' or 'config_linux'
+
 local config = {
   cmd = {
     'java', '-Declipse.application=org.eclipse.jdt.ls.core.id1',
@@ -10,7 +14,7 @@ local config = {
     '-Dlog.protocol=true', '-Dlog.level=ALL', '-Xms1g', '--add-modules=ALL-SYSTEM', '--add-opens',
     'java.base/java.util=ALL-UNNAMED', '--add-opens', 'java.base/java.lang=ALL-UNNAMED', '-jar',
     home .. '/.local/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
-    '-configuration', home .. '/.local/jdtls/config_mac/', '-data', workspace_dir,
+    '-configuration', home .. '/.local/jdtls/' .. config_dir, '-data', workspace_dir,
   },
   on_attach = function(_, bufnr)
     vim.api.nvim_create_autocmd('CursorHold', {
--- lua/vapour/plugins/init.lua
+++ lua/vapour/plugins/init.lua
@@ -1,13 +1,12 @@
 --------------------------------------Bootstrapping Packer----------------------------------
-local execute = vim.api.nvim_command
-
 local fn = vim.fn
 
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
-  execute 'packadd packer.nvim'
+  Packer_bootstrap = fn.system({
+    'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path,
+  })
 end
 
 local packer = Vapour.utils.plugins.require('packer')
@@ -19,9 +18,9 @@ packer.init(Vapour.plugins.packer.init)
 
 --------------------------------Autocommand for PackerSync-----------------------------------
 
-local plugin_path = fn.stdpath('config') .. '/lua/vapour/plugins/init.lua'
+--[[ local plugin_path = fn.stdpath('config') .. '/lua/vapour/plugins/init.lua'
 
---[[ local packer_augroup = vim.api.nvim_create_augroup('packer_user_config', {})
+local packer_augroup = vim.api.nvim_create_augroup('packer_user_config', {})
 vim.api.nvim_clear_autocmds({ group = packer_augroup, buffer = 0 })
 vim.api.nvim_create_autocmd('BufWritePost ', {
   pattern = plugin_path,
@@ -197,7 +196,7 @@ return packer.startup(function(use)
     'nvim-telescope/telescope.nvim',
     requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } },
     disable = not is_enabled('telescope'),
-    cmd = { 'Telescope', 'h Telescope' },
+    cmd = { 'Telescope', 'h telescope' },
     module = 'telescope',
     config = function()
       require('telescope-config')
@@ -340,5 +339,10 @@ return packer.startup(function(use)
   use { 'anuvyklack/keymap-layer.nvim' }
   use { 'jlanzarotta/bufexplorer' }
   use { 'sindrets/winshift.nvim' }
+  use { 'preservim/vim-markdown' }
+
   for _, plugin in pairs(Vapour.plugins.user) do use(plugin) end
+
+  if Packer_bootstrap then require('packer').sync() end
+
 end)
