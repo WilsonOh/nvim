66d241c58aa5b48e54768645edc9678f28b52db7
Wilson Oh <e0773510@u.nus.edu>
2022-08-07 23:08:29 +0800

    remove redundant default values from autopairs setup; add bandaid fix for .tsx file indent

--- lua/autopairs-config/init.lua
+++ lua/autopairs-config/init.lua
@@ -1,9 +1,4 @@
-require("nvim-autopairs").setup({
-	break_line_filetype = nil,
-	check_ts = true,
-	enable_check_bracket_line = true,
-	map_bs = true,
-})
+require("nvim-autopairs").setup({})
 
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp = require("cmp")
