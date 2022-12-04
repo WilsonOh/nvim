0a7e168f7b8fa5539500acd3b1bc98e9ecfaf85d
Wilson Oh <e0773510@u.nus.edu>
2022-12-04 14:34:03 +0800

    moved queries into after directory; added rust injection for doc test comments

--- after/queries/rust/injections.scm
+++ after/queries/rust/injections.scm
@@ -1,3 +1,5 @@
+; extends
+
 (macro_invocation
  (scoped_identifier
     path: (identifier) @_path (#eq? @_path "sqlx")
@@ -6,3 +8,10 @@
  (token_tree
    (raw_string_literal) @sql) 
    (#offset! @sql 1 0 0 0))
+
+((line_comment) @_first 
+ (_) @rust
+ (line_comment) @_last 
+ (#match? @_first "^/// ```$") 
+ (#match? @_last "^/// ```$")
+ (#offset! @rust 0 4 0 4))
