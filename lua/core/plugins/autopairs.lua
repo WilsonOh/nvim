1fe7aff513f251d14bf28234c4e0068d10322b73
Wilson Oh <e0773510@u.nus.edu>
2022-12-25 01:16:59 +0800

    moved cmp-autopairs snippet from autopairs.lua to cmp.lua

--- lua/core/plugins/autopairs.lua
+++ lua/core/plugins/autopairs.lua
@@ -7,10 +7,6 @@ M.config = function()
   local npairs = require("nvim-autopairs")
   npairs.setup({})
 
-  --[[ local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-  local cmp = require("cmp")
-  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done()) ]]
-
   local Rule = require("nvim-autopairs.rule")
 
   local brackets = { { "(", ")" }, { "[", "]" }, { "{", "}" } }
--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -13,6 +13,10 @@ local M = {
 }
 
 M.config = function()
+  local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+  local cmp = require("cmp")
+  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
   vim.o.completeopt = "menu,menuone,noselect"
 
   local luasnip = require("luasnip")
