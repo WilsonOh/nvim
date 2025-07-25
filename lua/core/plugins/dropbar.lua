b341a4b7bbb285c70ba25c0ff218ae13b3e61aec
Wilson Oh <wilson.ohyx@shopee.com>
2025-07-25 12:08:04 +0800

    fix formatting and disable vim-matchup treesitter integration

--- lua/core/plugins/dropbar.lua
+++ lua/core/plugins/dropbar.lua
@@ -1,20 +1,20 @@
 return {
-  'Bekaboo/dropbar.nvim',
+  "Bekaboo/dropbar.nvim",
   config = function()
-    local dropbar_api = require('dropbar.api')
-    vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
-    vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
-    vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    local dropbar_api = require("dropbar.api")
+    vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+    vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+    vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
 
     require("dropbar").setup({
       bar = {
         enable = function(buf, win, _)
           if
-              not vim.api.nvim_buf_is_valid(buf)
-              or not vim.api.nvim_win_is_valid(win)
-              or vim.fn.win_gettype(win) ~= ''
-              or vim.wo[win].winbar ~= ''
-              or vim.bo[buf].ft == 'help'
+            not vim.api.nvim_buf_is_valid(buf)
+            or not vim.api.nvim_win_is_valid(win)
+            or vim.fn.win_gettype(win) ~= ""
+            or vim.wo[win].winbar ~= ""
+            or vim.bo[buf].ft == "help"
           then
             return false
           end
@@ -28,15 +28,15 @@ return {
             return false
           end
 
-          return vim.bo[buf].bt == 'terminal'
-              or vim.bo[buf].ft == 'markdown'
-              or pcall(vim.treesitter.get_parser, buf)
-              or not vim.tbl_isempty(vim.lsp.get_clients({
-                bufnr = buf,
-                method = 'textDocument/documentSymbol',
-              }))
+          return vim.bo[buf].bt == "terminal"
+            or vim.bo[buf].ft == "markdown"
+            or pcall(vim.treesitter.get_parser, buf)
+            or not vim.tbl_isempty(vim.lsp.get_clients({
+              bufnr = buf,
+              method = "textDocument/documentSymbol",
+            }))
         end,
-      }
+      },
     })
   end,
   lazy = false,
